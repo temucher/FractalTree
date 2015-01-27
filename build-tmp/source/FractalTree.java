59b8d21a8023ac1d0ff3ca5e7087dec861d5ffaf
temucher <tjsmuch@gmail.com>
2015-01-27 11:17:54 -0800

    first commit

--- build-tmp/source/FractalTree.java
+++ build-tmp/source/FractalTree.java
@@ -1,6 +1,22 @@
-private double fractionLength = .8; 
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class FractalTree extends PApplet {
+
+private double fractionLength = .8f; 
 private int smallestBranch = 10; 
-private double branchAngle = .6;  
+private double branchAngle = .6f;  
 public void setup() 
 {   
 	size(640,480);    
@@ -34,3 +50,12 @@ public void drawBranches(int x,int y, double branchLength, double angle)
     }
 } 
 //tree curves off to the top right, something to do with y values in the recursive calls?
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "FractalTree" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
